c317564d6fd5aceb11aa452397b7613c107f66b3
Rithish03 <rithishrattand.21ece@kongu.edu>
2024-01-21 21:32:25 +0530

    This is second commit

---
+++ operators.js
@@ -0,0 +1,11 @@
+//Assignment operator
+var a = 30
+console.log("The output",a)
+//Arithmetic operator
+var a = 10
+var b = 20
+console.log("Output will be 30 and not 1010",a+b)
+console.log("Subtraction",a-b)
+console.log("Multiplication",a*b)
+console.log("Division",b/a)
+console.log("Modulus",a%b)
